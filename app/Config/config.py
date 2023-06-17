2494ec43483b06b4c609fe40200ff76857ab6ac7
Ivan Bogomolov <qhomop@gmail.com>
2023-06-17 19:24:29 +0300

    add base tg bot struct

--- app/Config/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-from telebot.async_telebot import AsyncTeleBot
-
-load_dotenv()
-
-bot: AsyncTeleBot = AsyncTeleBot(str(os.getenv("MAIN_BOT_TOKEN")))
-
-POSTGRES_URL: str = f'postgresql+asyncpg://{str(os.getenv("DATABASE_USER"))}:{str(os.getenv("DATABASE_PASSWORD"))}@{str(os.getenv("DATABASE_HOST"))}:{str(os.getenv("DATABASE_PORT"))}/{str(os.getenv("DATABASE_NAME"))}'
---
+++ app/Markups/__init__.py
@@ -0,0 +1,1 @@
+from .markupBuilder import MarkupBuilder
